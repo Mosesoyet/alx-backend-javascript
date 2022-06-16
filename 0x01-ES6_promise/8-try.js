fea105b409039b1c900f25c4010c7ca86d457870
Moses Oyet <trickmaxillar@gmail.com>
2022-06-16 04:33:43 +0300

    Add function

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,5 +1,5 @@
 /* test function */
-export default divideFunction(numerator, denominator) {
+export default function divideFunction(numerator, denominator) {
 	if (denominator == 0) {
 		throw new Error('cannot divide by 0');
 	}
