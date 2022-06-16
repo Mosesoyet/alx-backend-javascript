1430ecac40d9d4202a091b22c9cbc1397804f9b7
Moses Oyet <trickmaxillar@gmail.com>
2022-06-16 04:17:23 +0300

    Modify

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,20 @@
-import signUpUser from './4-user-promise.js'
-import uploadPhoto from './5-photo-reject.js'
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup( 
-	firstName, 
-	lastName, 
-	fileName
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
 ) {
-	const usrData = await signUpUser(firstName, lastName)
-	.then((data) => ({
-		status: 'fulfilled',
-		value: data,
-	}));
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: "fulfilled",
+    value: data,
+  }));
 
-	const imgData = await uploadPhoto(fileName)
-	.catch((err) => ({
-		status: 'rejected',
-		value: err.toString(),
-	}));
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: "rejected",
+    value: err.toString(),
+  }));
 
-	return Promise(usrData, imgData);
+  return Promise.resolve([userData, fileData]);
 }
