efd25c06776cd2bd36ce9f5524fe5a795bb7dd3f
Moses Oyet David <trickmaxillar@gmail.com>
2022-12-20 21:20:44 +0300

    Modify, add comment

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,6 +1,8 @@
-export default function concatArrays(array1, array2, string) {
-    for (const c of string) {
-        return c
-    } 
-    return array1 + array2 + c;
+/* eslint-disable */
+export default function concatArrays(
+    array1,
+    array2,
+    string
+    ) {
+        return [array1, array2, string];
 }
