ecc42b5b580e122fb2d14d1c40e93ad023f7c982
= <=>
2023-01-03 18:20:39 +0200

    remove the constant that holds array object

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
 /* eslint-disable */
 export default function getListStudents() {
-    const result = [
+    return [
         { id: 1, firstName: "Guillaume", location: "San Francisco" },
         { id: 2, firstName: "James", location: "Columbia" },
         { id: 3, firstName: "Serena", location: "San Francisco" },
     ];
-    return result;
 }
