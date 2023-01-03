53bb00fbe279d67b007ac0ec8925483d1207cc28
= <=>
2023-01-03 18:16:40 +0200

    Add data to array

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 export default function getListStudents( id, firstName, location ) {
     const result = [
-        { "id": id, "firstName": firstName, "location": location},
+        { id: 1, firstName: "Guillaume", location: "San Francisco" },
+        { id: 2, firstName: "James", location: "Columbia" },
+        { id: 3, firstName: "Serena", location: "San Francisco" },
     ];
     return result;
 }
