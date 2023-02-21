0738bedac1533a487177a20f37348d6c082d9e4c
= <=>
2023-02-21 16:53:21 +0000

    Fix

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,20 @@
 const http = require('http');
 
-const host = 'localhost';
-const port = '1245';
+const PORT = 1245;
+const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-	const response = "Hello Holberton School!";
+  const responseText = 'Hello Holberton School!';
 
-	res.setHeader('Content-Type', 'text/plain');
-        res.setHeader('Content-Length', responseText.length);
-        res.statusCode = 200;
-        res.write(Buffer.from(responseText));
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(port, host, () => {
-	process.stdout.write(`Server listening at -> http://${host}:${port}\n`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
