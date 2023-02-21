26655adac2ba5e7f20fa627e8d771ab1a1bad5a0
= <=>
2023-02-21 16:13:05 +0000

    Modify

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,7 +4,7 @@
  * @param {String} message The message to be displayed.
  * @author Moses Oyet <https://github.com/Mosesoyet>.
  */
-const displayMessage = (message) => {
+const displayMessage = ( message ) => {
 	console.log(message);
 };
 
