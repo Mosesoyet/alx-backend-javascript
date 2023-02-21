eaee1b794a949ed58afb0a263ac2a6754244db63
= <=>
2023-02-21 16:11:15 +0000

    a function that uses process

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+/**
+ * A program that prints to STDIN
+ */
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on('readable', () => {
+	const name = process.stdin.read();
+
+	if ( name ) {
+		process.stdout.write(`Your name is: ${ name }`);
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write("This important software is now closing\n");
+});
