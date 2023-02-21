896324739b0bb6b61b870fdcf7d81b49e96849d1
= <=>
2023-02-21 07:16:26 +0000

    Add a function to display text to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/*
+ * Display message on STDOUT
+ * @param {message} The message to be displayed
+ */
+
+const displayMessage = (message) => {
+	console.log(message)
+}
+
+module.exports = displayMessage;
