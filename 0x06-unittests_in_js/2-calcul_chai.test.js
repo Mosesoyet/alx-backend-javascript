afa7b0388aa9daeaeec96e12bf85319c59195e3e
= <=>
2023-02-24 13:37:01 +0000

    Add first test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,34 +1,15 @@
-const calculateNumber = require('./1-calcul.js');
-const assert = require("assert");
+const calculateNumber = require('./2-calcul_chai.js');
+const expect = require('chai').expect
 
 describe("calculateNumber", () => {
 
 	describe("SUM", () => {
 
 		it("Should sum two numbers and round off float numbers", () => {
-
-			assert.equal(6, calculateNumber('SUM',1.4,4.5));
-
-		});
-
-	});
-
-	describe("SUBTRACT", () => {
-
-		it("Should subtract two numbers, if float number then should round the folat", () => {
-
-			assert.equal(-4, calculateNumber('SUBTRACT', 1.4, 4.5));
+			
+			expect.equal(calculateNumber('SUM',1.4,4.5),6,"Success");
 
 		});
-	});
 
-	describe("DIVIDE", () => {
-
-		it("Should divide two numbers, if b is 0, should return error", () => {
-
-			assert.equal(0.2, calculateNumber('DIVIDE', 1.4, 4.5));
-			assert.equal("Error", calculateNumber('DIVIDE', 1.4, 0));
-
-		});
 	});
 });
