fa067d950f12cb1982417fb159cbc2d7e3a01b7e
= <=>
2023-02-24 16:38:40 +0000

    Copy previous file content

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,10 @@
+const Utils = require('./utils.js');
+
+function sendPaymentRequestToApi( totalAmount, totalShipping ) {
+
+	let total = Utils.calculateNumber( "SUM", totalAmount, totalShipping );
+	console.log(`The total is: ${total}`);
+
+}
+
+module.exports = sendPaymentRequestToApi;
