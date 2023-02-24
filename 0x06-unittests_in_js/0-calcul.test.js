791063f93f6e36a3d7679f755e0dbd5ba2309f4c
= <=>
2023-02-24 12:15:51 +0000

    Start first test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,12 +7,11 @@ const assert = require('assert');
 
 describe("calculateNumber", function () {
 
-	assert.equal(4, calculateNumber(1,3));
-	assert.equal(5, calculateNumber(1, 3.7));
-	assert.equal(5, calculateNumber(1.2,3.7));
-	assert.equal(6, calculateNumber(1.5,3.7));
-	assert.notEqual(5, calculateNumber(2.5, 2.5));
+	describe("sum numbers", () => {
 
-	});
+		it("Should sum two numbers and round off float to whole number", () => {
 
+			assert.equal(4, calculateNumber(1,3));
+
+		});
 });
