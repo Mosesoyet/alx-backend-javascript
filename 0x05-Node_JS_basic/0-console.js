b6b8eb36aac0eac93a479fcba0cbc13204b71b51
= <=>
2023-02-24 15:41:35 +0000

    Modify

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,11 +1,13 @@
-#!/usr/bin/node
 /*
  * Display message on STDOUT.
  * @param {String} message The message to be displayed.
  * @author Moses Oyet <https://github.com/Mosesoyet>.
  */
+
 const displayMessage = ( message ) => {
+
 	console.log(message);
-};
+
+}
 
 module.exports = displayMessage;
