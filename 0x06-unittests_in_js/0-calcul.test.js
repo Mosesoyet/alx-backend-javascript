f8b69ff39b4b18c519c638c005bdf4fd9c5aff28
= <=>
2023-02-24 12:11:31 +0000

    Remove other descriptions

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,19 +7,12 @@ const assert = require('assert');
 
 describe("calculateNumber", function () {
 
-	describe("float number", function () {
-
-		it("should return sum if float number is rounded", function () {
-
-			assert.equal(4, calculateNumber(1,3));
-			assert.equal(5, calculateNumber(1, 3.7));
-			assert.equal(5, calculateNumber(1.2,3.7));
-			assert.equal(6, calculateNumber(1.5,3.7));
-			assert.notEqual(5, calculateNumber(2.5, 2.5));
-
-		});
+	assert.equal(4, calculateNumber(1,3));
+	assert.equal(5, calculateNumber(1, 3.7));
+	assert.equal(5, calculateNumber(1.2,3.7));
+	assert.equal(6, calculateNumber(1.5,3.7));
+	assert.notEqual(5, calculateNumber(2.5, 2.5));
 
 	});
-
 
 });
