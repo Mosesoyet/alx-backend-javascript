01feddbef858d9de48ff6bc90b560677dbec0f64
= <=>
2023-02-24 16:33:51 +0000

    Modify

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,25 +1,23 @@
 const Utils = {
 	calculateNumber(type, a, b) {
-		
 		switch( type ) {
-			
 			case "SUM":
-				return (Math.round(a) + Math.round(b));
-			        break;
-			
-			case "SUBTRACT":
-				return (Math.round(a) - Math.round(b));
-			        break;
-			
-			case "DIVIDE":
-				if ( Math.round(b) === 0 ) {
-					return "Error";
-				} else {
-					return (Math.round(a) / Math.round(b));
-				};
-				break;
+			return (Math.round(a) + Math.round(b));
+			break;
+
+		        case "SUBTRACT":
+			return (Math.round(a) - Math.round(b));
+			break;
+
+		        case "DIVIDE":
+			if ( Math.round(b) === 0 ) {
+				return "Error";
+			} else {
+				return (Math.round(a) / Math.round(b));
+			};
+			break;
 		};
-	};
-}
+	},
+};
 
 module.exports = Utils;
